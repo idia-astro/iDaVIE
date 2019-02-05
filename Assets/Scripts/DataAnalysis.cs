b466e896b4a65a8fe189c6b1c1fac522928fcc71
sivi <alexandersivitilli@gmail.com>
2019-02-05 16:03:25 +0200

    Created value look-up method. Still needs testing.

--- Assets/Scripts/DataAnalysis.cs
+++ Assets/Scripts/DataAnalysis.cs
@@ -7,8 +7,13 @@ using UnityEngine;
 public class DataAnalysis
 {
     [DllImport("data_analysis_tool")]
-    public static extern void FindMaxMin(IntPtr dataPtr, long numberElements, out float maxResult, out float minResult);
+    public static extern int FindMaxMin(IntPtr dataPtr, long numberElements, out float maxResult, out float minResult);
 
-   [DllImport("data_analysis_tool")]
+    [DllImport("data_analysis_tool")]
     public static extern int NearNeighborScale(IntPtr dataPtr, out IntPtr newDataPtr, int dimX, int dimY, int dimZ, int windowX, int windowY, int windowZ);
+
+    [DllImport("data_analysis_tool")]
+    public static extern int GetVoxelValue(IntPtr dataPtr, out float voxelValue, int x, int y, int z);
+
+
 }
