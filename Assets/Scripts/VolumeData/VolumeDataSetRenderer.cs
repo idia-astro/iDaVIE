dc2193b64944eac744b7adb09cff82aabde9cfd4
veggiesaurus <accomrie@gmail.com>
2019-03-11 13:12:51 +0200

    switched to Vectrosity for line rendering, added cube outline/axes and voxel display

--- Assets/Scripts/VolumeData/VolumeDataSetRenderer.cs
+++ Assets/Scripts/VolumeData/VolumeDataSetRenderer.cs
@@ -1,4 +1,6 @@
+using System.Collections.Generic;
 using UnityEngine;
+using Vectrosity;
 
 namespace VolumeData
 {
@@ -52,9 +54,13 @@ namespace VolumeData
         public Vector3 InitialPosition { get; private set; }
         public Quaternion InitialRotation { get; private set; }
         public Vector3 InitialScale { get; private set; }
-        
+
         public float InitialThresholdMin { get; private set; }
         public float InitialThresholdMax { get; private set; }
+
+        public Vector3Int CursorVoxel { get; private set; }
+        public float CursorValue { get; private set; }
+        private VectorLine _voxelOutline, _cubeOutline;
 
         private MeshRenderer _renderer;
         private Material _materialInstance;
@@ -114,6 +120,53 @@ namespace VolumeData
             InitialRotation = transform.rotation;
             InitialThresholdMax = ThresholdMax;
             InitialThresholdMin = ThresholdMin;
+
+            // Bounding box outline and axes
+            Vector3 axesIndicatorOrigin = - 0.5f * Vector3.one;
+            Vector3 axesIndicatorOpposite = 0.5f * Vector3.one;
+            var cubeOutlinePoints = new List<Vector3> {
+                // axis indicators
+                axesIndicatorOrigin, axesIndicatorOrigin + Vector3.right,
+                axesIndicatorOrigin, axesIndicatorOrigin + Vector3.up, 
+                axesIndicatorOrigin, axesIndicatorOrigin + Vector3.forward,
+                // opposite axes
+                axesIndicatorOpposite, axesIndicatorOpposite - Vector3.right,
+                axesIndicatorOpposite, axesIndicatorOpposite - Vector3.up,
+                axesIndicatorOpposite, axesIndicatorOpposite - Vector3.forward,
+                // remaining vertices
+                axesIndicatorOrigin + Vector3.right, axesIndicatorOrigin + Vector3.right + Vector3.up,
+                axesIndicatorOrigin + Vector3.right, axesIndicatorOrigin + Vector3.right + Vector3.forward,
+                axesIndicatorOrigin + Vector3.up, axesIndicatorOrigin + Vector3.up + Vector3.right,
+                axesIndicatorOrigin + Vector3.up, axesIndicatorOrigin + Vector3.up + Vector3.forward,
+                axesIndicatorOrigin + Vector3.forward, axesIndicatorOrigin + Vector3.forward + Vector3.right,
+                axesIndicatorOrigin + Vector3.forward, axesIndicatorOrigin + Vector3.forward + Vector3.up
+            };
+
+            var defaultColor = Color.white;
+            var axesLineColors = new List<Color32>
+            {
+                new Color(1.0f, 0.4f, 0.4f),
+                new Color(0.4f, 1.0f, 0.4f),
+                new Color(0.4f, 0.4f, 1.0f),
+                defaultColor, defaultColor, defaultColor,
+                defaultColor, defaultColor,
+                defaultColor, defaultColor,
+                defaultColor, defaultColor
+            };
+            
+            _cubeOutline = new VectorLine("CubeAxes", cubeOutlinePoints, 2.0f, LineType.Discrete);
+            _cubeOutline.SetColors(axesLineColors);
+            _cubeOutline.drawTransform = transform;
+            _cubeOutline.Draw3DAuto();
+            
+            // Voxel axes
+            CursorVoxel = new Vector3Int(-1, -1, -1);
+            _voxelOutline = new VectorLine("VoxelOutline", new List<Vector3>(24), 1.0f);
+            _voxelOutline.MakeCube(Vector3.zero, 1, 1, 1);
+            _voxelOutline.drawTransform = transform;
+            _voxelOutline.color = Color.green;
+            _voxelOutline.active = false;
+            _voxelOutline.Draw3DAuto();
         }
 
         public void ShiftColorMap(int delta)
@@ -122,6 +175,39 @@ namespace VolumeData
             int currentIndex = ColorMap.GetHashCode();
             int newIndex = (currentIndex + delta + numColorMaps) % numColorMaps;
             ColorMap = ColorMapUtils.FromHashCode(newIndex);
+        }
+
+        public void SetCursorPosition(Vector3 cursor)
+        {
+            Vector3 objectSpacePosition = transform.InverseTransformPoint(cursor);
+            Bounds objectBounds = new Bounds(Vector3.zero, Vector3.one);
+            if (objectBounds.Contains(objectSpacePosition) && _dataSet != null)
+            {
+                Vector3 positionCubeSpace = new Vector3((objectSpacePosition.x + 0.5f) * _dataSet.XDim, (objectSpacePosition.y + 0.5f) * _dataSet.YDim, (objectSpacePosition.z + 0.5f) * _dataSet.ZDim);
+                Vector3 voxelCornerCubeSpace = new Vector3(Mathf.Floor(positionCubeSpace.x), Mathf.Floor(positionCubeSpace.y), Mathf.Floor(positionCubeSpace.z));
+                Vector3 voxelCenterCubeSpace = voxelCornerCubeSpace + 0.5f * Vector3.one;
+                Vector3Int newVoxelCursor = new Vector3Int(Mathf.RoundToInt(voxelCornerCubeSpace.x) + 1, Mathf.RoundToInt(voxelCornerCubeSpace.y) + 1, Mathf.RoundToInt(voxelCornerCubeSpace.z) + 1);
+
+                if (!newVoxelCursor.Equals(CursorVoxel))
+                {
+                    CursorVoxel = newVoxelCursor;
+                    CursorValue = _dataSet.GetValue(CursorVoxel.x, CursorVoxel.y, CursorVoxel.z);
+                    Vector3 voxelCenterObjectSpace = new Vector3(voxelCenterCubeSpace.x / _dataSet.XDim - 0.5f, voxelCenterCubeSpace.y / _dataSet.YDim - 0.5f, voxelCenterCubeSpace.z / _dataSet.ZDim - 0.5f);
+                    _voxelOutline.MakeCube(voxelCenterObjectSpace, 1.0f / _dataSet.XDim, 1.0f / _dataSet.YDim, 1.0f / _dataSet.ZDim);
+                }
+
+                _voxelOutline.active = true;
+            }
+            else
+            {
+                if (_voxelOutline != null && _voxelOutline.active)
+                {
+                    _voxelOutline.active = false;
+                }
+
+                CursorValue = float.NaN;
+                CursorVoxel = new Vector3Int(-1, -1, -1);
+            }            
         }
 
         // Update is called once per frame
