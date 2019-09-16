1db1079357824db5138461a1f5f598fb5ff1f6c8
CosmicElysium <alexandersivitilli@gmail.com>
2019-09-16 15:40:37 +0200

    Cube spins around all three axes and time stamps SteamVR system time to correspond with frame time in exported steamvr frame timing.

---
+++ Assets/BenchmarkManager.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using VolumeData;
+using UnityEngine;
+using Valve.VR;
+
+public class BenchmarkManager : MonoBehaviour
+{
+    private VolumeDataSetRenderer _testVolume;
+    private float _totalAngle = 0;
+    private int _numberRotations = 0;
+    private int _rotationAxis = 0;
+    private int _distanceSet = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        double timeInSeconds = 0;
+        var vr = SteamVR.instance;
+        if (vr != null)
+        {
+            var timing = new Valve.VR.Compositor_FrameTiming();
+            timing.m_nSize = (uint)System.Runtime.InteropServices.Marshal.SizeOf(typeof(Valve.VR.Compositor_FrameTiming));
+            vr.compositor.GetFrameTiming(ref timing, 0);
+
+            timeInSeconds = timing.m_flSystemTimeInSeconds;
+
+            /*
+        System.Diagnostics.PerformanceCounter perfUptimeCount = new System.Diagnostics.PerformanceCounter("System", "System Up Time");
+        System.TimeSpan uptimeSpan = System.TimeSpan.FromSeconds(perfUptimeCount.NextValue());
+        double unixVersion = uptimeSpan.TotalSeconds;
+        */
+        _testVolume = GetComponentInChildren<VolumeDataSetRenderer>();
+        
+        }
+        Debug.Log("Rotating around Y-axis at " + timeInSeconds + " seconds.");
+        _testVolume.transform.localRotation = new Quaternion(0, 0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //float prevAngle = _testVolume.transform.localRotation.eulerAngles.y;
+        if (_numberRotations < 2)
+        {
+            float deltaAngle = Time.deltaTime * 60;
+            switch (_rotationAxis)
+            {
+                case 0:
+                    _testVolume.transform.Rotate(0, deltaAngle, 0, Space.Self);
+                    break;
+                case 1:
+                   _testVolume.transform.Rotate(0, 0, deltaAngle, Space.Self);
+                    break;
+                case 2:
+                    _testVolume.transform.Rotate(deltaAngle, 0 , 0, Space.Self);
+                    break;
+            }
+            //float currentAngle = _testVolume.transform.localRotation.eulerAngles.y;
+            //float angleChange = Mathf.Abs(currentAngle - prevAngle);
+            _totalAngle += deltaAngle;
+            //Debug.Log("Rotation: " + _totalAngle);
+            _numberRotations = (int)(_totalAngle / 360f);
+            //Debug.Log("Rotation#: " + _numberRotations);
+        }
+        else
+        {
+            _totalAngle = 0;
+            _numberRotations = 0;
+            _testVolume.transform.localRotation = new Quaternion(0, 0, 0, 0);
+            //_testVolume.transform.Rotate(0, 0, 90, Space.Self);
+
+            switch (_rotationAxis)
+            {
+                case 0:
+                    _testVolume.transform.Rotate(-90, 0, 0, Space.Self);
+                    _rotationAxis++;
+                    Debug.Log("Rotating around Z-axis");
+                    break;
+                case 1:
+                    //_testVolume.transform.Rotate(90, 0, 0, Space.Self);
+                    _testVolume.transform.Rotate(0, 0, 90, Space.Self);
+                    _rotationAxis++;
+                    Debug.Log("Rotating around X-axis");
+                    break;
+
+                case 2:
+                    _rotationAxis = 0;
+                    _testVolume.transform.Translate(0, 0, 1);
+                    Debug.Log("Rotating around Y-axis");
+                    break;
+            }
+
+
+        }
+        
+    }
+}
