d8916d0d415092c96b6da6985239d6f3871ac134
CosmicElysium <alexandersivitilli@gmail.com>
2020-05-28 12:36:44 +0200

    Native Plugins build system changed to CMake (#75)
    * dll's can build successully with VS build tools, but not mingw because of xalan package compatibility.
    
    * Removed plugin debugger, but added the csharp script for future use.
    
    * built plugins load into unity and run, but need a few dependencies from msys2 to be loaded as well including libcurl, libwinpthread-1, and libstdc++-6. I will check later if all of these are actually needed.
    
    * Adjusted build paths
    
    * Update README.md
    
    Added new cmake build instructions
    
    * Added custom patch for mingw-cfitsio package that allows large file input. This patch must be applied using instructions to be added in README
    
    * Update README.md
    
    Included cfitsio patch instructions
    
    * Removed debugging options from PKGBUILD
    
    * Updated build locations for plugins and instructions. Also added some gitignore files.
    
    * Removed old native_plugins directory that used visual studio build system
    
    * Updated readme with fixes to build process
    
    * Another fix to build instructions
    
    * Update README.md
    
    Fixed the binary directory for MinGW
    
    * Made cfitsio linking static and added an install target to cmake build that adds plugins to Unity assets. msys64\mingw64\lib may need to be added to PATH
    
    * Dependency DLL's are copied over to Plugins directory with install target. dll's in mingw directory should not be referenced with unity anymore.
    
    * minor readme adjustment and version bump
    
    Co-authored-by: Angus Comrie <accomrie@gmail.com>

--- Assets/Scripts/PluginInterface/FitsReader.cs
+++ Assets/Scripts/PluginInterface/FitsReader.cs
@@ -16,82 +16,82 @@ public class FitsReader
             return FitsOpenFileReadWrite(out fptr, filename, out status);
     }
 
-    [DllImport("fits_reader")]
+    [DllImport("libfits_reader")]
     public static extern int FitsOpenFileReadOnly(out IntPtr fptr, string filename, out int status);
 
-    [DllImport("fits_reader")]
+    [DllImport("libfits_reader")]
     public static extern int FitsOpenFileReadWrite(out IntPtr fptr, string filename, out int status);
 
-    [DllImport("fits_reader")]
+    [DllImport("libfits_reader")]
     public static extern int FitsCreateFile(out IntPtr fptr, string filename, out int status);
 
-    [DllImport("fits_reader")]
+    [DllImport("libfits_reader")]
     public static extern int FitsCloseFile(IntPtr fptr, out int status);
 
-    [DllImport("fits_reader")]
+    [DllImport("libfits_reader")]
     public static extern int FitsFlushFile(IntPtr fptr, out int status);
 
-    [DllImport("fits_reader")]
+    [DllImport("libfits_reader")]
     public static extern int FitsMovabsHdu(IntPtr fptr, int hdunum, out int hdutype, out int status);
 
-    [DllImport("fits_reader")]
+    [DllImport("libfits_reader")]
     public static extern int FitsGetNumHeaderKeys(IntPtr fptr, out int keysexist, out int morekeys, out int status);
 
-    [DllImport("fits_reader")]
+    [DllImport("libfits_reader")]
     public static extern int FitsGetNumRows(IntPtr fptr, out long nrows, out int status);
 
-    [DllImport("fits_reader")]
+    [DllImport("libfits_reader")]
     public static extern int FitsGetNumCols(IntPtr fptr, out int ncols, out int status);
 
-    [DllImport("fits_reader")]
+    [DllImport("libfits_reader")]
     public static extern int FitsGetImageDims(IntPtr fptr, out int dims, out int status);
 
-    [DllImport("fits_reader")]
+    [DllImport("libfits_reader")]
     public static extern int FitsGetImageSize(IntPtr fptr, int dims, out IntPtr naxes, out int status);
 
-    [DllImport("fits_reader")]
+    [DllImport("libfits_reader")]
     public static extern int FitsCreateImg(IntPtr fptr, int bitpix, int naxis, IntPtr naxes, out int status);
 
-    [DllImport("fits_reader")]
+    [DllImport("libfits_reader")]
     public static extern int FitsCopyHeader(IntPtr infptr, IntPtr outfptr, out int status);
 
-    [DllImport("fits_reader")]
+    [DllImport("libfits_reader")]
     public static extern int FitsWriteImageInt16(IntPtr fptr, int dims, long nelements, IntPtr array, out int status);
 
-    [DllImport("fits_reader")]
+    [DllImport("libfits_reader")]
     public static extern int FitsWriteKey(IntPtr fptr, int datatype, string keyname, IntPtr value, string comment, out int status);
 
-    [DllImport("fits_reader")]
+    [DllImport("libfits_reader")]
     public static extern int FitsUpdateKey(IntPtr fptr, int datatype, string keyname, IntPtr value, string comment, out int status);
 
-    [DllImport("fits_reader")]
+    [DllImport("libfits_reader")]
     public static extern int FitsMakeKeyN(string keyroot, int value, StringBuilder keyname, out int status);
 
-    [DllImport("fits_reader")]
+    [DllImport("libfits_reader")]
     public static extern int FitsReadKey(IntPtr fptr, int datatype, string keyname, StringBuilder colname, IntPtr comm, out int status);
 
-    [DllImport("fits_reader")]
+    [DllImport("libfits_reader")]
     public static extern int FitsReadKeyN(IntPtr fptr, int keynum, StringBuilder keyname, StringBuilder keyvalue, StringBuilder comment, out int status);
 
-    [DllImport("fits_reader")]
+    [DllImport("libfits_reader")]
     public static extern int FitsReadColFloat(IntPtr fptr, int colnum, long firstrow, long firstelem, long nelem, out IntPtr array, out int status);
 
-    [DllImport("fits_reader")]
+    [DllImport("libfits_reader")]
     public static extern int FitsReadColString(IntPtr fptr, int colnum, long firstrow, long firstelem, long nelem, out IntPtr ptrarray, out IntPtr chararray, out int status);
 
-    [DllImport("fits_reader")]
+    [DllImport("libfits_reader")]
     public static extern int FitsReadImageFloat(IntPtr fptr, int dims, long nelem, out IntPtr array, out int status);
 
-    [DllImport("fits_reader")]
+    [DllImport("libfits_reader")]
     public static extern int FitsReadImageInt16(IntPtr fptr, int dims, long nelem, out IntPtr array, out int status);
 
-    [DllImport("fits_reader")]
+    [DllImport("libfits_reader")]
     public static extern int CreateEmptyImageInt16(long sizeX, long sizeY, long sizeZ, out IntPtr array);
 
-    [DllImport("fits_reader")]
+    [DllImport("libfits_reader")]
     public static extern int FreeMemory(IntPtr pointerToDelete);
 
-    [DllImport("fits_reader")]
+    [DllImport("libfits_reader")]
     public static extern int InsertSubArrayInt16(IntPtr mainArray, long mainArraySize, IntPtr subArray, long subArraySize, long startIndex);
 
     public static IDictionary<string, string> ExtractHeaders(IntPtr fptr, out int status)
--- Assets/Scripts/PluginInterface/VOTableReader.cs
+++ Assets/Scripts/PluginInterface/VOTableReader.cs
@@ -34,55 +34,55 @@ public class VOTableReader
     }
 
 
-    [DllImport("votable_reader")]
+    [DllImport("libvotable_reader")]
     public static extern int VOTableInitialize(out IntPtr table_ptr);
 
-    [DllImport("votable_reader")]
+    [DllImport("libvotable_reader")]
     public static extern int VOTableOpenFile(IntPtr table_ptr, string filename, string xpath, out int status);
 
-    [DllImport("votable_reader")]
+    [DllImport("libvotable_reader")]
     public static extern int VOTableGetMetaData(IntPtr table_ptr, out IntPtr meta_ptr, out int status);
 
-    [DllImport("votable_reader")]
+    [DllImport("libvotable_reader")]
     public static extern int VOTableGetName(IntPtr table_ptr, out IntPtr name_ptr, out int status);
 
-    [DllImport("votable_reader")]
+    [DllImport("libvotable_reader")]
     public static extern int VOTableGetTableData(IntPtr table_ptr, out IntPtr data_ptr, out int status);
 
-    [DllImport("votable_reader")]
+    [DllImport("libvotable_reader")]
     public static extern int TableDataGetNumRows(IntPtr data_ptr, out int nrows, out int status);
 
-    [DllImport("votable_reader")]
+    [DllImport("libvotable_reader")]
     public static extern int TableDataGetRow(IntPtr data_ptr, out IntPtr row_ptr, int rowIndex, out int status);
 
-    [DllImport("votable_reader")]
+    [DllImport("libvotable_reader")]
     public static extern int RowGetColumn(IntPtr row_ptr, out IntPtr column_ptr, int colIndex, out int status);
 
-    [DllImport("votable_reader")]
+    [DllImport("libvotable_reader")]
     public static extern int ColumnGetFloatArray(IntPtr col_ptr, out IntPtr float_array, out int numElements, out int status);
 
-    [DllImport("votable_reader")]
+    [DllImport("libvotable_reader")]
     public static extern int ColumnGetInttArray(IntPtr col_ptr, out IntPtr int_array, out int numElements, out int status);
 
-    [DllImport("votable_reader")]
+    [DllImport("libvotable_reader")]
     public static extern int MetaDataGetNumCols(IntPtr meta_ptr, out int ncols, out int status);
 
-    [DllImport("votable_reader")]
+    [DllImport("libvotable_reader")]
     public static extern int MetaDataGetField(IntPtr meta_ptr, out IntPtr field_ptr, int fieldNum, out int status);
 
-    [DllImport("votable_reader")]
+    [DllImport("libvotable_reader")]
     public static extern int FieldGetName(IntPtr field_ptr, out IntPtr name_ptr, out int status);
 
-    [DllImport("votable_reader")]
+    [DllImport("libvotable_reader")]
     public static extern int FieldGetDataType(IntPtr field_ptr, out int datatype, out int status);
 
-    [DllImport("votable_reader")]
+    [DllImport("libvotable_reader")]
     public static extern int ColumnGetFloatArray(IntPtr col_ptr, IntPtr float_array, out int number_elements, out int status);
 
-    [DllImport("votable_reader")]
+    [DllImport("libvotable_reader")]
     public static extern int ColumnGetCharArray(IntPtr column_ptr, out IntPtr name_ptr, out int number_elements, out int status);
 
-    [DllImport("votable_reader")]
+    [DllImport("libvotable_reader")]
     public static extern int FreeMemory(IntPtr ptr);
 
 
