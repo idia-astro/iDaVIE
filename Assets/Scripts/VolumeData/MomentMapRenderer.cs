bb1cef400bd85c9cf8254d0c0de1631e41c68a57
Angus Comrie <accomrie@gmail.com>
2020-10-23 16:56:24 +0200

    colormapping compute kernel

--- Assets/Scripts/VolumeData/MomentMapRenderer.cs
+++ Assets/Scripts/VolumeData/MomentMapRenderer.cs
@@ -6,6 +6,7 @@ namespace VolumeData
     public class MomentMapRenderer : MonoBehaviour
     {
         public RenderTexture Moment0Map { get; private set; }
+        public RenderTexture ImageOutput { get; private set; }
 
         public Texture3D DataCube
         {
@@ -19,7 +20,7 @@ namespace VolumeData
                 }
             }
         }
-        
+
         public Texture3D MaskCube
         {
             get => _maskCube;
@@ -41,10 +42,11 @@ namespace VolumeData
 
         private ComputeShader _computeShader;
         private float _cachedMomentMapThreshold = Single.NaN;
-        private int _kernelIndex, _kernelIndexMasked;
+        private int _kernelIndex, _kernelIndexMasked, _colormapKernelIndex;
         private uint _kernelThreadGroupX, _kernelThreadGroupY;
         private Texture3D _dataCube;
         private Texture3D _maskCube;
+        private Texture2D _colormapTexture;
 
         private struct MaterialID
         {
@@ -53,13 +55,21 @@ namespace VolumeData
             public static readonly int MomentResult = Shader.PropertyToID("MomentResult");
             public static readonly int Threshold = Shader.PropertyToID("Threshold");
             public static readonly int Depth = Shader.PropertyToID("Depth");
+
+            public static readonly int ClampMin = Shader.PropertyToID("ClampMin");
+            public static readonly int ClampMax = Shader.PropertyToID("ClampMax");
+            public static readonly int OutputTexture = Shader.PropertyToID("OutputTexture");
+            public static readonly int ColormapTexture = Shader.PropertyToID("ColormapTexture");
+            public static readonly int ColormapOffset = Shader.PropertyToID("ColormapOffset");
         }
 
         private void Start()
         {
+            _colormapTexture = (Texture2D) Resources.Load("allmaps");
             _computeShader = (ComputeShader) Resources.Load("MomentMapGenerator");
-            _kernelIndex = _computeShader.FindKernel("MomentZeroGenerator");
-            _kernelIndexMasked = _computeShader.FindKernel("MaskedMomentZeroGenerator");
+            _kernelIndex = _computeShader.FindKernel("MomentOneGenerator");
+            _kernelIndexMasked = _computeShader.FindKernel("MaskedMomentOneGenerator");
+            _colormapKernelIndex = _computeShader.FindKernel("LinearColormap");
             uint temp;
             // This assumes the masked and unmasked kernels use the same group size. They really should, though
             _computeShader.GetKernelThreadGroupSizes(_kernelIndex, out _kernelThreadGroupX, out _kernelThreadGroupY, out temp);
@@ -87,7 +97,7 @@ namespace VolumeData
 
             bool maskActive = _maskCube && UseMask;
             int activeKernelIndex = maskActive ? _kernelIndexMasked : _kernelIndex;
-            
+
             _cachedMomentMapThreshold = threshold;
 
             if (!Moment0Map || (Moment0Map.width != _dataCube.width || Moment0Map.height != _dataCube.height))
@@ -99,6 +109,13 @@ namespace VolumeData
                 Moment0Map.useMipMap = false;
                 Moment0Map.filterMode = FilterMode.Point;
                 Moment0Map.Create();
+
+                ImageOutput?.Release();
+                ImageOutput = new RenderTexture(_dataCube.width, _dataCube.height, 0);
+                ImageOutput.enableRandomWrite = true;
+                ImageOutput.useMipMap = false;
+                ImageOutput.filterMode = FilterMode.Point;
+                ImageOutput.Create();
             }
 
             // Update shader variables
@@ -112,12 +129,14 @@ namespace VolumeData
             {
                 _computeShader.SetFloat(MaterialID.Threshold, _cachedMomentMapThreshold);
             }
+
             _computeShader.SetInt(MaterialID.Depth, _dataCube.depth);
-            
+
             // Run compute shader in tiles, based on kernel's group size
             int threadGroupsX = Mathf.CeilToInt(_dataCube.width / ((float) (_kernelThreadGroupX)));
             int threadGroupsY = Mathf.CeilToInt(_dataCube.height / ((float) (_kernelThreadGroupY)));
             _computeShader.Dispatch(activeKernelIndex, threadGroupsX, threadGroupsY, 1);
+
             return true;
         }
 
@@ -125,7 +144,20 @@ namespace VolumeData
         {
             if (Moment0Map != null)
             {
-                GUI.DrawTexture(new Rect(0, 0, Moment0Map.width * 3, Moment0Map.height * 3), Moment0Map);
+                // Run colormapping compute shader
+                _computeShader.SetTexture(_colormapKernelIndex, MaterialID.MomentResult, Moment0Map);
+                _computeShader.SetTexture(_colormapKernelIndex, MaterialID.OutputTexture, ImageOutput);
+                _computeShader.SetTexture(_colormapKernelIndex, MaterialID.ColormapTexture, _colormapTexture);
+                
+                // Default MomentOne bounds: 0 -> D - 1
+                _computeShader.SetFloat(MaterialID.ClampMin, 0.0f);
+                _computeShader.SetFloat(MaterialID.ClampMax, DataCube.depth - 1);
+                float offset = (ColorMapEnum.Turbo.GetHashCode() + 0.5f) / ColorMapUtils.NumColorMaps;
+                _computeShader.SetFloat(MaterialID.ColormapOffset, offset);
+                int threadGroupsX = Mathf.CeilToInt(_dataCube.width / ((float) (_kernelThreadGroupX)));
+                int threadGroupsY = Mathf.CeilToInt(_dataCube.height / ((float) (_kernelThreadGroupY)));
+                _computeShader.Dispatch(_colormapKernelIndex, threadGroupsX, threadGroupsY, 1);
+                GUI.DrawTexture(new Rect(0, 0, Moment0Map.width * 3, Moment0Map.height * 3), ImageOutput);
             }
         }
     }
