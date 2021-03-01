5e03810ddfc3ff22c23ae62534b72d249dea1aea
Fabio Roberto Vitello <fabio.vitello@inaf.it>
2021-03-01 12:40:08 +0100

    First Implementation

---
+++ Assets/Scripts/UI/KeypadController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KeypadController : MonoBehaviour
+{
+    /*
+    public enum KeyId
+    {
+
+        Delete = -1,
+        Zero = 0,
+        One = 1,
+        Two = 2,
+        Three = 3,
+        Four = 4,
+        Five = 5,
+        Six = 6,
+        Seven = 7,
+        Eight = 8,
+        Nine = 9      
+
+    }
+    */
+
+    public Text previewText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Confirm()
+    {
+        float value = float.Parse(previewText.text, System.Globalization.CultureInfo.InvariantCulture.NumberFormat);
+        Debug.Log(value);
+    }
+
+    public void KeyPressed(int key_id)
+    {
+
+        string text = previewText.text;
+        if (key_id == -1)
+            text = text.Remove(text.Length - 1);
+        else if (key_id == -2)
+            text = text + ".";
+        else
+            text = text + key_id.ToString();
+        
+        previewText.text = text;
+    }
+}
