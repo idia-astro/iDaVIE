7ad7318e86f76ba56b01e69ea7332c9684770d33
Fabio Vitello <fvitello@users.noreply.github.com>
2021-07-26 13:38:43 +0200

    issue #205 cleanup voice command list  creator and fixed rotation

--- Assets/Scripts/VoiceCommands/VoiceCommandListCreator.cs
+++ Assets/Scripts/VoiceCommands/VoiceCommandListCreator.cs
@@ -22,79 +22,11 @@ public class VoiceCommandListCreator : MonoBehaviour
 
     private VolumeCommandController _volumeCommandController;
 
-    /*
-    public GameObject volumeDatasetRendererObj = null;
-    public GameObject SofiaNewListController = null;
-
-    private FeatureSetManager featureSetManager;
-    private FeatureSetRenderer[] featureSetRenderer;
-    private int numberFeatureSetRenderers;
-
-    private int numberOfItems;
-
-    */
+  
     // Start is called before the first frame update
     void Start()
     {
-        /*
-
-                if (volumeDatasetRendererObj != null)
-                    _dataSets = volumeDatasetRendererObj.GetComponentsInChildren<VolumeDataSetRenderer>(true);
-
-
-                var firstActive = getFirstActiveDataSet();
-                if (firstActive && _activeDataSet != firstActive)
-                {
-                    _activeDataSet = firstActive;
-                }
-
-                if (_activeDataSet != null)
-                {
-                    featureSetManager = _activeDataSet.GetComponentInChildren<FeatureSetManager>();
-                  //  featureSetManager.ImportFeatureSet();
-                }
-
-                featureSetRenderer = featureSetManager.GetComponentsInChildren<FeatureSetRenderer>();
-                numberFeatureSetRenderers = featureSetRenderer.Length;
-                numberOfItems = featureSetRenderer[numberFeatureSetRenderers - 1].FeatureList.Count;
-                //Debug.Log($"Number of items: {numberOfItems}");
-
-                //setContent Holder Height;
-                content.sizeDelta = new Vector2(0, numberOfItems * 100);
-
-
-                for (int i = 0; i < numberOfItems; i++)
-                {
-                    // 100 Height of item
-                    float spawnY = i * 100;
-                    //newSpawn Position
-                    Vector3 pos = new Vector3(SpawnPoint.position.x+300, -spawnY, SpawnPoint.position.z);
-                    //instantiate item
-                    GameObject SpawnedItem = Instantiate(item, pos, SpawnPoint.rotation);
-                    //setParent
-                    SpawnedItem.transform.SetParent(SpawnPoint, false);
-                    //get ItemDetails Component
-                    SofiaListItem itemDetails = SpawnedItem.GetComponent<SofiaListItem>();
-
-                    itemDetails.SofiaNewListController = SofiaNewListController;
-                    //set name
-                    itemDetails.idTextField.text = (i+1).ToString();
-
-                    itemDetails.feature = featureSetRenderer[numberFeatureSetRenderers - 1].FeatureList[i];
-
-                    if (!itemDetails.feature.Visible)
-                        itemDetails.ToggleVisibility();
-
-                    itemDetails.sourceName.text = itemDetails.feature.Name;
-
-
-                    if (i%2!=0)
-                        itemDetails.GetComponent<Image>().color = new Color(0.4039216f, 0.5333334f, 0.5882353f, 1f);
-
-
-
-                }
-        */
+       
 
         _volumeCommandController = FindObjectOfType<VolumeCommandController>();
 
@@ -106,7 +38,7 @@ public class VoiceCommandListCreator : MonoBehaviour
             //newSpawn Position
             Vector3 pos = new Vector3(SpawnPoint.position.x + 300, -spawnY, SpawnPoint.position.z);
             //instantiate item
-            GameObject SpawnedItem = Instantiate(item, pos, SpawnPoint.rotation);
+            GameObject SpawnedItem = Instantiate(item, pos, Quaternion.identity);
             //setParent
             SpawnedItem.transform.SetParent(SpawnPoint, false);
 
