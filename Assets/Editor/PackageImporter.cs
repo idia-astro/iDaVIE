aa1db5b5b5ebce38221aaf119d2854ab0e89307a
CosmicElysium <alexandersivitilli@gmail.com>
2022-01-20 17:39:52 +0200

    Added batch file and unity script to automate build

---
+++ Assets/Editor/PackageImporter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+
+public class PackageImporter : MonoBehaviour
+{
+    public static void ImportPackages()
+    {
+        var projectDir = Directory.GetParent(Application.dataPath);
+        var packageDir = $"{projectDir.ToString()}/plugin_build";
+        Debug.Log($"Looking for packages in {packageDir}");
+        var packageNames = Directory.GetFiles(packageDir, "*.unitypackage");
+        Debug.Log($"Found {packageNames.Length} packages");
+        foreach (var package in packageNames)
+        {
+            Debug.Log($"Importing {package}");
+            UnityEditor.AssetDatabase.ImportPackage(package, false);
+        }
+    }
+}
