d8b2678e0628be7de1fd572bb69de5de2b119dd4
Cilliers Pretorius <pietersieliepcp@gmail.com>
2023-03-07 13:38:45 +0200

    Major autobuild step completed.
     * Autobuild completed with hardcoded vcpkg.cmake and Unity.exe locations, will add CLI arguments for that.
    
     * Unity asset packages downloaded, extracted and imported automatically.
    
     * Application is automatically built to ../build folder.
    
     * Still an issue regarding textmeshpro and importing the TMP Essentials - that requires the unity editor to run.

---
+++ build.ps1
@@ -0,0 +1,141 @@
+Write-Progress "Building native plugins..."
+Set-Location native_plugins_cmake
+$BuildFolderName = "build"
+if (Test-Path $BuildFolderName)
+{
+    Write-Host "build folder Exists"
+}
+else
+{
+    New-Item $BuildFolderName -ItemType Directory
+    Write-Host "build folder created successfully"
+}
+Set-Location $BuildFolderName
+$VCPKGCMAKE = "C:\Users\Public\IT\vcpkg\scripts\buildsystems\vcpkg.cmake"
+if (-not ((Test-Path $VCPKGCMAKE) -and (Test-Path -Path $VCPKGCMAKE -PathType leaf)))
+{
+    Write-Host "vcpkg's Cmake file can't be found at $VCPKGCMAKE, exiting..." -ForegroundColor Red
+	Set-Location ../..
+	exit
+}
+cmake --fresh -DCMAKE_TOOLCHAIN_FILE="$VCPKGCMAKE" -DCMAKE_BUILD_TYPE=Release ../
+cmake --build . --config Release --target install
+Write-Host "Native plugins built!"
+Set-Location ../..
+$PluginBuildFolderName = "plugin_build"
+if (Test-Path $PluginBuildFolderName)
+{
+    Write-Host "plugin build folder exists"
+}
+else
+{
+    New-Item $PluginBuildFolderName -ItemType Directory
+    Write-Host "plugin build folder created successfully"
+}
+Set-Location $PluginBuildFolderName
+
+Write-Host "Downloading packages..."
+Write-Progress -Activity "Downloading packages..." -Status "0% complete" -PercentComplete 0
+if (Test-Path ".\steamvr.unitypackage")
+{
+	Write-Progress "steamvr.unitypackage exists already" -Status "25% complete" -PercentComplete 25
+}
+else
+{
+	Write-Progress "fetching steamvr.unitypackage... " -Status "0% complete" -PercentComplete 0
+	Invoke-WebRequest https://github.com/ValveSoftware/steamvr_unity_plugin/releases/download/2.7.3/steamvr_2_7_3.unitypackage -OutFile steamvr.unitypackage
+	Write-Progress "Done." -Status "25% complete" -PercentComplete 25
+}
+
+if (Test-Path ".\nuget.unitypackage")
+{
+	Write-Progress "nuget.unitypackage exists already" -Status "50% complete" -PercentComplete 50
+}
+else
+{
+	Write-Progress "fetching nuget.unitypackage... " -Status "25% complete" -PercentComplete 25
+	Invoke-WebRequest https://github.com/GlitchEnzo/NuGetForUnity/releases/download/v3.0.5/NugetForUnity.3.0.5.unitypackage -OutFile nuget.unitypackage
+	Write-Progress "Done." -Status "50% complete" -PercentComplete 50
+}
+
+if (Test-Path ".\scroll_rect.unitypackage")
+{
+	Write-Progress "scroll_rect.unitypackage exists already" -Status "75% complete" -PercentComplete 75
+}
+else
+{
+	Write-Progress "fetching scroll_rect.unitypackage... " -Status "50% complete" -PercentComplete 50
+	Invoke-WebRequest https://github.com/CosmicElysium/Recyclable-Scroll-Rect/releases/download/v1.0/recyclable-scroll-rect.unitypackage -OutFile scroll_rect.unitypackage
+	Write-Progress "Done." -Status "75% complete" -PercentComplete 75
+}
+
+if (Test-Path ".\file_browser.unitypackage")
+{
+	Write-Progress "file_browser.unitypackage exists already" -Status "100% complete" -PercentComplete 100
+}
+else
+{
+	Write-Progress "fetching file_browser.unitypackage... " -Status "75% complete" -PercentComplete 75
+	Invoke-WebRequest https://github.com/gkngkc/UnityStandaloneFileBrowser/releases/download/1.2/StandaloneFileBrowser.unitypackage -OutFile file_browser.unitypackage
+	Write-Progress "Done." -Status "100% complete" -PercentComplete 100
+}
+
+Write-Host "Packages downloaded!"
+Write-Host "Importing packages into project..."
+if (Test-Path ".\unitypackage_extractor-x64.zip")
+{
+	Write-Progress "unitypackage_extractor-x64.zip exists already" -Status "100% complete" -PercentComplete 100
+}
+else
+{
+	Write-Progress "fetching unitypackage_extractor-x64.zip... "
+	Invoke-WebRequest https://github.com/Cobertos/unitypackage_extractor/releases/download/v1.1.0/unitypackage_extractor-x64.zip -OutFile unitypackage_extractor-x64.zip
+	Write-Progress "Done." -Status "100% complete" -PercentComplete 100
+}
+
+Expand-Archive .\unitypackage_extractor-x64.zip -DestinationPath .
+Remove-Item .\unitypackage_extractor-x64.zip
+if (Test-Path "Assets")
+{
+    Write-Host "extract target folder Exists"
+}
+else
+{
+    New-Item "Assets" -ItemType Directory
+    Write-Host "extract target folder created successfully"
+}
+
+Write-Progress "Extracting steamvr.unitypackage... " -Status "0% complete" -PercentComplete 0
+Start-Process ".\extractor.exe" -Wait -ArgumentList ".\steamvr.unitypackage ."
+Write-Progress "steamvr.unitypackage extracted"
+
+Write-Progress "Extracting nuget.unitypackage... " -Status "25% complete" -PercentComplete 25
+Start-Process ".\extractor.exe" -Wait -ArgumentList ".\nuget.unitypackage ."
+Write-Progress "nuget.unitypackage extracted"
+
+Write-Progress "Extracting file_browser.unitypackage... " -Status "50% complete" -PercentComplete 50
+Start-Process ".\extractor.exe" -Wait -ArgumentList ".\file_browser.unitypackage ."
+Write-Progress "file_browser.unitypackage extracted"
+
+Write-Progress "Extracting scroll_rect.unitypackage... " -Status "75% complete" -PercentComplete 75
+Start-Process ".\extractor.exe" -Wait -ArgumentList ".\scroll_rect.unitypackage ."
+Write-Progress "scroll_rect.unitypackage extracted" -Status "100% complete" -PercentComplete 100
+
+#Start-Process "C:\Users\Public\IT\Unity\2021.3.17f1\Editor\Unity.exe" -Wait -ArgumentList "-projectPath $PSScriptRoot -batchmode -nographics -logfile $PSScriptRoot\import.log -executeMethod AutoBuilder.ImportPackages -quit"
+Copy-Item -Path ".\Assets\*" -Destination "..\Assets" -Recurse
+#Write-Host "Packages imported."
+Set-Location ..
+Write-Host "Building player..."
+$GameBuildFolderName = "..\build"
+if (Test-Path $GameBuildFolderName)
+{
+    Write-Host "build folder Exists"
+}
+else
+{
+    New-Item $GameBuildFolderName -ItemType Directory
+    Write-Host "build folder created successfully"
+}
+#Start-Process "C:\Users\Public\IT\Unity\2021.3.17f1\Editor\Unity.exe" -Wait -ArgumentList "-projectPath $PSScriptRoot -batchmode -nographics -ignorecompilererrors -logfile $PSScriptRoot\build.log -executeMethod AutoBuilder.buildApplicationDev -quit"
+Start-Process "C:\Users\Public\IT\Unity\2021.3.17f1\Editor\Unity.exe" -Wait -ArgumentList "-projectPath $PSScriptRoot -batchmode -nographics -ignorecompilererrors -logfile $PSScriptRoot\build.log -buildWindows64Player $GameBuildFolderName\i-DaVIE-v.exe -quit"
+Write-Host "Finished!"
