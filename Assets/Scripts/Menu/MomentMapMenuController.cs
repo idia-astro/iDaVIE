606de1146b8d6a0f882c0e27381aac01690b0c12
Pietersielie <pietersieliepcp@gmail.com>
2024-05-27 15:32:29 +0200

    Keypad change added for moment map threshold.
      * Doesn't work, can't figure out why.
      * Not sure how it worked to begin with.

--- Assets/Scripts/Menu/MomentMapMenuController.cs
+++ Assets/Scripts/Menu/MomentMapMenuController.cs
@@ -13,14 +13,19 @@ using VolumeData;
 public class MomentMapMenuController : MonoBehaviour
 {
 
-    public Text ThresholdTypeText;
-    public Text LimitTypeText;
+    public TMP_Text ThresholdTypeText;
+    public TMP_Text LimitTypeText;
+
+    public TMP_Text ThresholdValueText;
     public TMP_Text MomentMap0Title;
     public TMP_Text MomentMap1Title;
     public GameObject volumeDataSetManager;
     private VolumeDataSetRenderer[] dataSets;
+    private float _threshold;
+    private float _cached_threshold;
     public Camera MomentMapCamera;
     int thresholdType = 0;
+    [SerializeField] private GameObject keypadPrefab = null;
     public enum ThresholdType
     {
         Mask, Threshold
@@ -44,16 +49,16 @@ public class MomentMapMenuController : MonoBehaviour
                 VolumeDataSet dataSet = getFirstActiveDataSet().GetDataSet();
                 getFirstActiveDataSet().GetMomentMapRenderer().UpdatePlotWindow();
 
-                LimitTypeText.gameObject.GetComponent<Text>().text = $"{_limitType}";
+                LimitTypeText.text = $"{_limitType}";
 
                 if (getFirstActiveDataSet().Mask != null)
                 {
-                    ThresholdTypeText.gameObject.GetComponent<Text>().text = (ThresholdType)0 + "";
+                    ThresholdTypeText.text = (ThresholdType)0 + "";
                     this.gameObject.transform.Find("Main_container").gameObject.transform.Find("Line_Threshold").gameObject.SetActive(false);
                 }
                 else
                 {
-                    ThresholdTypeText.gameObject.GetComponent<Text>().text = (ThresholdType)1 + "";
+                    ThresholdTypeText.text = (ThresholdType)1 + "";
                     this.gameObject.transform.Find("Main_container").gameObject.transform.Find("Line_Threshold").gameObject.SetActive(true);
                 }
                 MomentMap0Title.text += $" ({dataSet.GetPixelUnit()})";
@@ -65,18 +70,32 @@ public class MomentMapMenuController : MonoBehaviour
         }
     }
 
+    void Update()
+    {
+        _threshold = float.Parse(ThresholdValueText.text, System.Globalization.CultureInfo.InvariantCulture.NumberFormat);
+        if (_cached_threshold != _threshold)
+        {
+            SetMomentMapThreshold();
+        }
+    }
+
     public void IncreaseMomentMapThreshold()
     {
-        if (getFirstActiveDataSet().GetMomentMapRenderer().MomentMapThreshold <= 0.1)
-        {
-            getFirstActiveDataSet().GetMomentMapRenderer().MomentMapThreshold += getFirstActiveDataSet().GetMomentMapRenderer().momstep;
-        }
+        _threshold += getFirstActiveDataSet().GetMomentMapRenderer().momstep;
     }
 
     public void DecreaseMomentMapThreshold()
     {
-        if (getFirstActiveDataSet().GetMomentMapRenderer().MomentMapThreshold >= -0.1)
-            getFirstActiveDataSet().GetMomentMapRenderer().MomentMapThreshold -= getFirstActiveDataSet().GetMomentMapRenderer().momstep;
+        _threshold -= getFirstActiveDataSet().GetMomentMapRenderer().momstep;
+    }
+
+    public void SetMomentMapThreshold()
+    {
+        _cached_threshold = _threshold;
+        if (getFirstActiveDataSet().GetMomentMapRenderer() != null)
+        {
+            getFirstActiveDataSet().GetMomentMapRenderer().MomentMapThreshold = _threshold;
+        }
     }
 
     private VolumeDataSetRenderer getFirstActiveDataSet()
@@ -108,7 +127,7 @@ public class MomentMapMenuController : MonoBehaviour
 
         }
         getFirstActiveDataSet().GetMomentMapRenderer().CalculateMomentMaps();
-        ThresholdTypeText.gameObject.GetComponent<Text>().text = (ThresholdType)thresholdType + "";
+        ThresholdTypeText.text = (ThresholdType)thresholdType + "";
     }  
 
     public void SaveToImage()
@@ -208,68 +227,32 @@ public class MomentMapMenuController : MonoBehaviour
         ToastNotification.ShowSuccess($"Moment map 0 saved to {path0}");
         ToastNotification.ShowSuccess($"Moment map 1 saved to {path1}");
     }
-    
-    public IntPtr RenderTextureToArray(RenderTexture renderTexture)
+
+    /// <summary>
+    /// Opens a keypad in VR space. Once the user is satisfied with the number, it sends the value back to target.
+    /// </summary>
+    /// <param name="target">The text field that the keypad number will be sent to.</param>
+    public void OpenKeypad(TextMeshProUGUI target)
     {
-        IntPtr arrayToReturn = Marshal.AllocHGlobal(renderTexture.width * renderTexture.height * sizeof(float));
-        
-        // Create a new Texture2D and set its pixel values
-        Texture2D tex = new Texture2D(renderTexture.width, renderTexture.height, TextureFormat.RFloat, false);
-        RenderTexture.active = renderTexture;
-        tex.ReadPixels(new Rect(0, 0, renderTexture.width, renderTexture.height), 0, 0);
-        tex.Apply();
-        RenderTexture.active = null;
-
-        // Get the raw texture data
-        var data = tex.GetRawTextureData<float>();
-
-        Marshal.Copy(data.ToArray(), 0, arrayToReturn, data.Length);
-        
-        // Return the array
-        return arrayToReturn;
-    }
-    
-    public void SaveToFits()
-    {
-        var directory = new DirectoryInfo(Application.dataPath);
-        var directoryPath = Path.Combine(directory.Parent.FullName, "Outputs/MomentMaps");
-        try
+        // If any keypads are open, destroy them first.
+        if (GameObject.FindGameObjectWithTag("keypad") != null)
         {
-            if (!Directory.Exists(directoryPath))
-            {
-                Directory.CreateDirectory(directoryPath);
-            }
+            GameObject[] keypads = GameObject.FindGameObjectsWithTag("keypad");
+            foreach(GameObject kp in keypads)
+                Destroy(kp);
         }
-        catch (IOException ex)
-        {
-            Console.WriteLine(ex.Message);
-        }
-        var path0 = Path.Combine(directoryPath, string.Format("Moment_map_0_{0}.fits", DateTime.Now.ToString("yyyyMMdd_Hmmss")));
-        var path1 = Path.Combine(directoryPath, string.Format("Moment_map_1_{0}.fits", DateTime.Now.ToString("yyyyMMdd_Hmmss")));
-
-        var moment0Array = RenderTextureToArray(getFirstActiveDataSet().GetMomentMapRenderer().Moment0Map);
-        var moment1Array = RenderTextureToArray(getFirstActiveDataSet().GetMomentMapRenderer().Moment1Map);
-
-        IntPtr mainFitsFilePtr = IntPtr.Zero;
-        FitsReader.FitsOpenFile(out mainFitsFilePtr, getFirstActiveDataSet().FileName, out int status, true);
-
-        FitsReader.WriteMomentMap(mainFitsFilePtr, path0, moment0Array,
-            getFirstActiveDataSet().GetMomentMapRenderer().Moment0Map.width,
-            getFirstActiveDataSet().GetMomentMapRenderer().Moment0Map.height);
-        FitsReader.WriteMomentMap(mainFitsFilePtr, path1, moment1Array,
-            getFirstActiveDataSet().GetMomentMapRenderer().Moment1Map.width,
-            getFirstActiveDataSet().GetMomentMapRenderer().Moment1Map.height);
-        
-        FitsReader.FitsCloseFile(mainFitsFilePtr, out status);
-        
-        Debug.Log($"Moment maps saved to {path0} and {path1}");
+        Vector3 pos = new Vector3(this.transform.parent.position.x, this.transform.parent.position.y - 0.4f, this.transform.parent.position.z);
+        //instantiate item
+        GameObject SpawnedItem = Instantiate(keypadPrefab, pos, this.transform.parent.rotation);
+        SpawnedItem.transform.localRotation = this.transform.parent.rotation;
+        SpawnedItem.GetComponent<KeypadController>().targetText = target;
     }
     
     public void ChangeLimitType()
     {
         _limitType = _limitType == LimitType.MinMax ? LimitType.ZScale : LimitType.MinMax;
         getFirstActiveDataSet().GetMomentMapRenderer().UseZScale = _limitType == LimitType.ZScale;
-        LimitTypeText.gameObject.GetComponent<Text>().text = $"{_limitType}";
+        LimitTypeText.text = $"{_limitType}";
     }
 
 }
