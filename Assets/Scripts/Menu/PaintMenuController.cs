36f191da37381c215e803fa24cac2473529134fd
Alex Sivitilli <alexandersivitilli@gmail.com>
2024-07-02 15:54:22 +0200

    Fix issue with new mask saving asking to overwrite

--- Assets/Scripts/Menu/PaintMenuController.cs
+++ Assets/Scripts/Menu/PaintMenuController.cs
@@ -222,23 +222,37 @@ public class PaintMenuController : MonoBehaviour
 
     public void SaveMask()
     {
-        savePopup.transform.SetParent(this.transform.parent, false);
-        savePopup.transform.localPosition = this.transform.localPosition;
-        savePopup.transform.localRotation = this.transform.localRotation;
-        savePopup.transform.localScale = this.transform.localScale;
+        if (_activeDataSet.IsMaskNew)
+        {
+            SaveNewMask();
+        }
+        else
+        {
+            savePopup.transform.SetParent(this.transform.parent, false);
+            savePopup.transform.localPosition = this.transform.localPosition;
+            savePopup.transform.localRotation = this.transform.localRotation;
+            savePopup.transform.localScale = this.transform.localScale;
 
-        savePopup.transform.Find("Content").gameObject.transform.Find("FirstRow").gameObject.transform.Find("Cancel").GetComponent<Button>().onClick.RemoveAllListeners();
-        savePopup.transform.Find("Content").gameObject.transform.Find("FirstRow").gameObject.transform.Find("Overwrite").GetComponent<Button>().onClick.RemoveAllListeners();
-        savePopup.transform.Find("Content").gameObject.transform.Find("FirstRow").gameObject.transform.Find("NewFile").GetComponent<Button>().onClick.RemoveAllListeners();
+            savePopup.transform.Find("Content").gameObject.transform.Find("FirstRow").gameObject.transform
+                .Find("Cancel").GetComponent<Button>().onClick.RemoveAllListeners();
+            savePopup.transform.Find("Content").gameObject.transform.Find("FirstRow").gameObject.transform
+                .Find("Overwrite").GetComponent<Button>().onClick.RemoveAllListeners();
+            savePopup.transform.Find("Content").gameObject.transform.Find("FirstRow").gameObject.transform
+                .Find("NewFile").GetComponent<Button>().onClick.RemoveAllListeners();
 
-        savePopup.transform.Find("Content").gameObject.transform.Find("FirstRow").gameObject.transform.Find("Cancel").GetComponent<Button>().onClick.AddListener(SaveCancel);
-        savePopup.transform.Find("Content").gameObject.transform.Find("FirstRow").gameObject.transform.Find("Overwrite").GetComponent<Button>().onClick.AddListener(SaveOverwriteMask);
-        savePopup.transform.Find("Content").gameObject.transform.Find("FirstRow").gameObject.transform.Find("NewFile").GetComponent<Button>().onClick.AddListener(SaveNewMask);
+            savePopup.transform.Find("Content").gameObject.transform.Find("FirstRow").gameObject.transform
+                .Find("Cancel").GetComponent<Button>().onClick.AddListener(SaveCancel);
+            savePopup.transform.Find("Content").gameObject.transform.Find("FirstRow").gameObject.transform
+                .Find("Overwrite").GetComponent<Button>().onClick.AddListener(SaveOverwriteMask);
+            savePopup.transform.Find("Content").gameObject.transform.Find("FirstRow").gameObject.transform
+                .Find("NewFile").GetComponent<Button>().onClick.AddListener(SaveNewMask);
 
-        _volumeInputController.InteractionStateMachine.Fire(VolumeInputController.InteractionEvents.PaintModeDisabled);
-         this.gameObject.SetActive(false);
-        savePopup.SetActive(true);
-        
+            _volumeInputController.InteractionStateMachine.Fire(VolumeInputController.InteractionEvents
+                .PaintModeDisabled);
+            this.gameObject.SetActive(false);
+            savePopup.SetActive(true);
+        }
+
     }
 
     public void SaveCancel()
