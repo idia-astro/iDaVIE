ca29493805856122bed5affd3a7fd577c27fde10
Mayhew <mayhrunes@gmail.com>
2025-06-20 17:01:33 +0200

    Add frame capture to VideoMaker (WIP)

--- Assets/Scripts/VideoMaker/VideoCameraController.cs
+++ Assets/Scripts/VideoMaker/VideoCameraController.cs
@@ -1,9 +1,11 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
 
 namespace VideoMaker
 {
@@ -36,6 +38,8 @@ namespace VideoMaker
 
         public TMP_Text StatusText;
 
+        private Camera _camera;
+
         private const float Dist = 1.5f;
 
         private VideoPositionAction[] _positionActionArray = {
@@ -53,10 +57,20 @@ namespace VideoMaker
         private VideoDirectionAction[] _directionActionArray = {
             new VideoDirectionActionLookAt(5f, new Vector3(0f, 0f, 0f))
         };
-        private VideoDirectionAction[] _upDirectionActionArray= {
+        private VideoDirectionAction[] _upDirectionActionArray = {
             new VideoDirectionActionHold(4.5f, Vector3.up),
             new VideoDirectionActionTween(0.5f, Vector3.up, new Vector3(1f, 0f, 0f))
         };
+
+        private int _frameRate = 10;
+        private int _frameCounter = 0;
+        private Queue<byte[]> _frameQueue = new();
+
+        private Thread _exportThread;
+        private bool _threadIsProcessing;
+        private bool _terminateThreadWhenDone;
+
+        private string _directoryPath;
 
         private List<VideoPositionAction> _positionActionQueue;
         private List<VideoDirectionAction> _directionActionQueue;
@@ -77,6 +91,17 @@ namespace VideoMaker
             enabled = false;
             _targetCube = GameObject.Find("TestCube");
             _targetCube.SetActive(false);
+
+            var directory = new DirectoryInfo(Application.dataPath);
+            _directoryPath = Path.Combine(directory.Parent.FullName, "Outputs/Video");
+
+            if (!System.IO.Directory.Exists(_directoryPath))
+            {
+                System.IO.Directory.CreateDirectory(_directoryPath);
+            }
+
+            _camera = GetComponent<Camera>();
+            _camera.enabled = false;
         }
 
         void Update()
@@ -91,31 +116,46 @@ namespace VideoMaker
                 _upDirectionActionQueue[0].GetDirection(_timeUpDirection, position)
             );
 
-            //TODO: For preview mode use Time.deltaTime, for recordings use the defined frame time of the VideoScript
-            float deltaTime = Time.deltaTime;
+            float deltaTime = 0f;
 
+            switch (_status)
+            {
+                case Status.PreviewPlayback:
+                    deltaTime = Time.deltaTime;
+                    break;
+                case Status.ExportPlayback:
+                    deltaTime = 1f / (float)_frameRate;
+                    //Write texture to queue. Should I wait till end of frame?
+                    // _camera.Render();
+                    // // Make a new texture and read the active Render Texture into it.
+                    // Texture2D tex = new Texture2D(_camera.targetTexture.width, _camera.targetTexture.height, TextureFormat.RGB24, false);
+                    // tex.ReadPixels(new Rect(0, 0, _camera.targetTexture.width, _camera.targetTexture.height), 0, 0);
+                    // tex.Apply();
+                    // // Encode texture into PNG
+                    // byte[] bytes = tex.EncodeToPNG();
+                    // Destroy(tex); //Is it better to re-use the texture each frame?
+                    // _frameQueue.Enqueue(bytes);
+                    break;
+            }
+            
             _time += deltaTime;
 
             //TODO is a final frame necessary?
-
             if (!UpdateActionTime(ref _timePosition, ref _positionActionQueue, deltaTime))
             {
-                enabled = false;
-                ProgressBar.SetActive(false);
+                TaskComplete();
                 return;
             }
 
             if (!UpdateActionTime(ref _timeDirection, ref _directionActionQueue, deltaTime))
             {
-                enabled = false;
-                ProgressBar.SetActive(false);
+                TaskComplete();
                 return;
             }
 
             if (!UpdateActionTime(ref _timeUpDirection, ref _upDirectionActionQueue, deltaTime))
             {
-                enabled = false;
-                ProgressBar.SetActive(false);
+                TaskComplete();
                 return;
             }
         }
@@ -159,7 +199,6 @@ namespace VideoMaker
             return true;
         }
 
-
         private void UpdateTransform(Vector3 position, Vector3 direction, Vector3 upDirection)
         {
             position = _cubeTransform.TransformPoint(position);
@@ -170,8 +209,10 @@ namespace VideoMaker
             gameObject.transform.LookAt(position + direction, upDirection);
         }
 
-        public void OnPreviewClick()
+        public void StartPlayback()
         {
+            StatusText.text = _statusLabels[_status];
+
             _cubeTransform = null;
             _targetCube.SetActive(false);
 
@@ -214,13 +255,120 @@ namespace VideoMaker
             _duration = Math.Min(_duration, duration);
 
             enabled = true;
-            StatusText.text = _statusLabels[Status.PreviewPlayback];
             ProgressBar.SetActive(true);
 
             _time = 0f;
             _timePosition = 0f;
             _timeDirection = 0f;
             _timeUpDirection = 0f;
+        }
+
+        public void OnPreviewClick()
+        {
+            if (_status != Status.Idle)
+            {
+                return;
+            }
+            _status = Status.PreviewPlayback;
+            _camera.enabled = true;
+            StartPlayback();
+        }
+
+        public void OnRecordClick()
+        {
+            if (_status != Status.Idle)
+            {
+                return;
+            }
+            // Kill the encoder thread if running from a previous execution
+            if (_exportThread != null && (_threadIsProcessing || _exportThread.IsAlive)) {
+                _threadIsProcessing = false;
+                _exportThread.Join();
+            }
+
+                _status = Status.ExportPlayback;
+            _frameCounter = 0;
+            _terminateThreadWhenDone = false;
+            _threadIsProcessing = true;
+            _exportThread = new Thread(SaveFrames);
+            _exportThread.Start();
+            //TODO: Does the Thread terminate when callback is complete?
+            
+            _camera.enabled = true;
+
+            StartPlayback();
+        }
+
+        private void TaskComplete() {
+            switch (_status)
+            {
+                case Status.PreviewPlayback:
+                    enabled = false;
+                    _camera.enabled = false;
+                    ProgressBar.SetActive(false);
+                    _status = Status.Idle;
+                    break;
+                case Status.ExportPlayback:
+                    enabled = false;
+                    _camera.enabled = false;
+                    _terminateThreadWhenDone = true;
+                    if (!_threadIsProcessing)
+                    {
+                        ProgressBar.SetActive(false);
+                        _status = Status.Idle; //TODO move to next stage of export
+                    }
+                    break;
+            }
+        }
+
+        private void OnRenderImage(RenderTexture source, RenderTexture destination)
+        {
+            Graphics.Blit(source, destination);
+            if (_status != Status.ExportPlayback)
+            {
+                return;
+            }
+
+            // Make a new texture and read the active Render Texture into it.
+            Texture2D tex = new Texture2D(destination.width, destination.height, TextureFormat.RGB24, false);
+            tex.ReadPixels(new Rect(0, 0, destination.width, destination.height), 0, 0);
+            tex.Apply();
+            // Encode texture into PNG
+            byte[] bytes = tex.EncodeToPNG();
+            Destroy(tex); //Is it better to re-use the texture each frame?
+            _frameQueue.Enqueue(bytes);
+
+            // Graphics.Blit(source, destination);
+        }
+
+        private void SaveFrames()
+        {
+            while (_threadIsProcessing)
+            {
+                if (_frameQueue.Count > 0)
+                {
+                    string path = Path.Combine(
+                        _directoryPath,
+                        "frame" + _frameCounter.ToString() + ".png" //TODO change to bmp for better video
+                    );
+                    //TODO is a FileStream better here?
+                    //TODO does this work with .bmp format?
+                    File.WriteAllBytes(path, _frameQueue.Dequeue());
+                    _frameCounter++;
+                }
+                else
+                {
+                    if (_terminateThreadWhenDone)
+                    {
+                        break;
+                    }
+                    Thread.Sleep(1);
+                }
+            }
+
+            // _terminateThreadWhenDone = false;
+            _threadIsProcessing = false;
+            TaskComplete(); //Can only call this on the  main thread, what to do?
         }
     }
 }
